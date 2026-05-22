0696f1e565b94edbaa79b294ab188d60ec1cf6c2
Никита <chernyshovnikita290507@mail.ru>
2026-05-22 18:02:54 +0300

    admin_part_fix

--- config.py
+++ config.py
@@ -4,15 +4,5 @@ import os
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
-ADMIN_ID = [1143200581, 1094046982]
+ADMIN_ID = [1143200581, 8989109361]
 ADMIN_USERNAME = ['@Nekitnnn', '@Gods_GG']
-
-commands = {
-    '/start': ('Запустить бота',),
-    '/help': ('Показать это сообщение',),
-    '/add_primogems': (
-        'Добавить примогемы',
-    ),
-    '/wishes': ('Показать историю круток',),
-    '/shop': ('Показать магазин',),
-}
--- routers/admin.py
+++ routers/admin.py
@@ -1,10 +1,19 @@
-from email.mime import message
 from aiogram.filters import Command
-from database import *
-from aiogram import Bot, Dispatcher, types, Router
-from services.commands import *
+from aiogram import types, Router, F
+from config import ADMIN_ID
+
 router = Router()
 
-@router.message()
-async def admin_commands_handler(message: types.Message):
-    await admin_commands(message)
+
+@router.message(F.from_user.id.in_(ADMIN_ID), Command("help"))
+async def cmd_admin_help(message: types.Message):
+    help_text = """
+    🛡️ Админ команды:
+    /add_primogems <user_id> <amount> - Добавить примогемы
+    /reduce_primogems <user_id> <amount> - Уменьшить примогемы
+    /ban <user_id> <hours> - Забанить пользователя
+    /unban <user_id> - Разбанить пользователя
+    /delete_user <user_id> - Удалить пользователя
+    /get_user <user_id> - Информация о пользователе
+    """
+    await message.answer(help_text)
